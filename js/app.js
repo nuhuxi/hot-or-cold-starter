9d984cf72b6f175e187709a8a57097bc183c84e4
nuhuxi <mark.keckeis@gmail.com>
2014-04-02 10:35:31 -0700

    first commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 
 $(document).ready(function(){
-	
+
+	$("#guessButton").click(function(){
+		/*Get the user input */
+		var inputString = $("#userGuess").val();
+		var inputNumber = parseInt(inputString);
+		var numGuesses = 0;
+		var isItDecimal = inputString.indexOf(".");
+
+		validateNumber(isItDecimal, inputNumber);
+
+		/* generate a random number*/
+		var randomNumber = Math.floor((Math.random()*100)+1);
+
+		/* get the users input and make sure it is in the range of possible numbers*/
+
+	});
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -11,6 +26,30 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	function validateNumber(isItDecimal,inputNumber){
+  		if (isItDecimal==-1) {
+			if(!isNaN(inputNumber) && inputNumber!==''){
+					if(inputNumber > 0 && inputNumber < 100){
+						console.log('Good number!,' + 'inputNumber=' + inputNumber);
+					} else{
+						invalidInput();
+					}
+			}
+			else{
+				console.log(isNaN(inputNumber));
+				invalidInput();
+			}
+		}else{
+			invalidInput();
+		}
+
+  	};
+
+  	function invalidInput(){
+  		alert("Enter a number between 0 and 99 - Decimal point is not allowed!");
+  	};
+
 
 });
 
