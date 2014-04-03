c52a4d40c672127af1c07069db419c57c4d91637
nuhuxi <mark.keckeis@gmail.com>
2014-04-03 11:18:08 -0700

    Should be done

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-	var randomNumber = '';
-	var numGuesses = 0;
-	var inputString;
-	var theGuess;
-	var isItDecimal;
-	var lastNumberGuessed;
-
+	var randomNumber = '',
+		numGuesses = 0,
+		inputString,
+		theGuess,
+		isItDecimal,
+		lastNumberGuessed;
+	console.log('Resetting game at the top of the page');
 	resetGame();
 
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
+  	$(".what").click(function () {
     	$(".overlay").fadeIn(1000);
   	});
 
   	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
+  	$("a.close").click(function () {
   		$(".overlay").fadeOut(1000);
   	});
 
   	/*--- User makes a guess ---*/
-	$("#guessButton").click(function(event){
+	$("#guessButton").click(function (event) {
 		event.preventDefault();
 		numGuesses++;
 		$('#count').text(numGuesses);
@@ -39,7 +39,7 @@ $(document).ready(function(){
 	$('.new').click(resetGame);
 
 	/*--- FUNCTIONS ---*/
-  	function resetGame(){
+  	function resetGame() {
   		/*--- Reset the variables --*/
 		randomNumber = '';
 		numGuesses = 0;
@@ -54,7 +54,7 @@ $(document).ready(function(){
 		console.log('The random number is '+ randomNumber);
 	};
 
-  	function validateNumber(isItDecimal,inputNumber){
+  	function validateNumber(isItDecimal,inputNumber) {
   		if (isItDecimal==-1) {
 			if(!isNaN(inputNumber) && inputNumber!==''){
 					if(inputNumber > 0 && inputNumber < 101){
@@ -70,34 +70,63 @@ $(document).ready(function(){
 		}
   	};
 
-  	function invalidInput(){
+  	function invalidInput() {
   		alert("Enter a number between 1 and 100 - Decimal point is not allowed!");
   	};
 
-  	function isTheGuessRight(){
-  		console.log('The guess is ' +theGuess);
-  		console.log('The last guess was '+lastNumberGuessed);s
-  		if(numGuesses===1){
-  			alert('Nice first guess');
-  		}
+  	function isTheGuessRight() {
+  		console.log('The guess is ' + theGuess);
 
+		/* The following nested if statement is complicated. It would have been better done with a "case" statement
+			but that is not available in javascript. 
 
-  		if(theGuess===randomNumber){
-  			alert('You did it!!!!');
-  			resetGame();
-  		}else{
-  			if(theGuess<randomNumber && lastNumberGuessed>theGuess){
-  				console.log('You are getting warmer');
-  			}else if(randomNumber<theGuess && theGuess<lastNumberGuessed){
-  				  		console.log('You are getting warmer');
-  					}else{
-  						if (randomNumber<theGuess && randomNumber>lastNumberGuessed) {
-  						console.log('You are getting colder')
-  						}else{	
-  						  console.log('You are getting colder');
-  							}
-  					}
-  		};
+			There are 6 cases for user guesses as follows:
+
+			-Case 1 : Winner
+			-Case 2 : Guess is not within 70 of the number
+			-Case 3 : Guess is not within 50 of the number
+			-Case 4 : Guess is not within 30 of the number
+			-Case 5 : Guess is not within 10 of the number
+			-Case 6 : Guess is within 5 of the number
+
+		*/
+
+		if(Math.abs(randomNumber-theGuess)===0) { /* Case 1 */
+			$("#feedback").text("You WIN!!!");
+			document.getElementById("feedback").style.backgroundColor ="#FF3A69";	
+		}else{ 
+			if(Math.abs(randomNumber-theGuess) > 70) { /* Case 2 */
+				$("#feedback").text("Brrr...Cold");
+				document.getElementById("feedback").style.backgroundColor ="#5132CC";	
+
+			}else{ 
+				if(Math.abs(randomNumber-theGuess) > 50) { /* Case 3 */
+					$("#feedback").text("Pretty cold");
+					document.getElementById("feedback").style.backgroundColor ="#3270CC";	
+
+				}else { /* else number 3 */
+					if(Math.abs(randomNumber-theGuess) > 30) {/* Case 4 */
+						$("#feedback").text("Warmer...");
+						document.getElementById("feedback").style.backgroundColor ="#32CC32";
+ 
+					}else { /* else number 4 */
+
+						if(Math.abs(randomNumber-theGuess) > 10) {/* Case 5 */
+							$("#feedback").text('Really warm...');
+							document.getElementById("feedback").style.backgroundColor ="#CC3282";
+
+						}else { /* Case 6*/
+							if(Math.abs(randomNumber-theGuess) < 5) {/* Case 5 */
+								$("#feedback").text("Smokin' HOT!!!");
+								document.getElementById("feedback").style.backgroundColor ="#CC3257";
+							}
+
+						} 
+					} 
+				} 
+			}
+		} 
+	};
 });
 
 
